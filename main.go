f50fe73413a3810e4835d416ac9d86dd57a78faf
parscr <parscalculator@gmail.com>
2021-02-26 14:49:45 +0330

    pcr

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+     name := "World"
+    
     return &events.APIGatewayProxyResponse{
-        StatusCode:        200,
-        Headers:           map[string]string{"Content-Type": "text/plain"},
-        MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-        Body:              "Hello, World!",
-        IsBase64Encoded:   false,
+        StatusCode:        302,
+        Headers:           map[string]string{"Location": "https://google.com", "Cache-Control": "no-cache"},
+        Body:       "Hi " + name,
     }, nil
 }
 

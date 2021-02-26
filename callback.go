273def2038d7a23fdc464cbf125cfd4aa99ad007
parscr <parscalculator@gmail.com>
2021-02-26 17:47:46 +0330

    pcr

--- callback.go
+++ callback.go
@@ -13,18 +13,11 @@ import (
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
     params := request.QueryStringParameters
-   url := "https://api.idpay.ir/v1.1/payment"
-
-
+  url := "https://api.idpay.ir/v1.1/payment/verify"
 
 data := map[string]string{
+  "id":       "d2e353189823079e1e4181772cff5292",
   "order_id": "101",
-  "amount":   params["amount"],
-  "name":     params["name"],
-  "phone":    params["phone"],
-  "mail":     params["mail"],
-  "desc":     params["desc"],
-  "callback": "https://angry-albattani-4d4bde.netlify.app/.netlify/functions/callback",
 }
 
 payload, _ := json.Marshal(data)
@@ -33,24 +26,17 @@ req, _ := http.NewRequest("POST", url, bytes.NewBuffer(payload))
 
 req.Header.Set("Content-Type", "application/json")
 req.Header.Set("X-API-KEY", "6a7f99eb-7c20-4412-a972-6dfb7cd253a4")
-req.Header.Set("X-SANDBOX", "1")
+req.Header.Set("X-SANDBOX", 1)
 
 res, _ := http.DefaultClient.Do(req)
 
 defer res.Body.Close()
 body, _ := ioutil.ReadAll(res.Body)
 
-strbody:= string(body)
-type nova struct {
-    Id    string
-    Link  string
-}
-var ret nova
-json.Unmarshal([]byte(strbody), &ret)
-fmt.Printf("id: %s, link: %s", ret.Id, ret.Link)
+fmt.Println(string(body))
 return &events.APIGatewayProxyResponse{
         StatusCode:        302,
-        Headers:           map[string]string{"Location": ret.Link , "Content-Type": "text/plain"},
+        Headers:           map[string]string{"Location": "https://google.com" , "Content-Type": "text/plain"},
         MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
         Body:              "Hello, World!",
         IsBase64Encoded:   false,

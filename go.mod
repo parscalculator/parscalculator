17a839c91aed2c5609ef1912149aed0343597a6e
parscr <parscalculator@gmail.com>
2021-02-26 15:45:32 +0330

    p

---
+++ go.mod
@@ -0,0 +1,5 @@
+go 1.16
+
+module github.com/aws/aws-lambda-go/events
+
+module github.com/aws/aws-lambda-go/lambda
--- main.go
+++ main.go
@@ -22,25 +22,6 @@ data := map[string]string{
   "callback": "https://example.com/callback",
 }
 
-payload, _ := json.Marshal(data)
-
-req, _ := http.NewRequest("POST", url, bytes.NewBuffer(payload))
-
-req.Header.Set("Content-Type", "application/json")
-req.Header.Set("X-API-KEY", "6a7f99eb-7c20-4412-a972-6dfb7cd253a4")
-req.Header.Set("X-SANDBOX", "1")
-
-res, _ := http.DefaultClient.Do(req)
-
-defer res.Body.Close()
-body, _ := ioutil.ReadAll(res.Body)
-    return &events.APIGatewayProxyResponse{
-        StatusCode:        302,
-        Headers:           map[string]string{"Location": "https://google.com" , "Content-Type": "text/plain"},
-        MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-        Body:              "Hello, World!",
-        IsBase64Encoded:   false,
-    }, nil
 }
 
 func main() {

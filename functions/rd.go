f85d55bcc4fa6a612b10bab4ca85f268886fb8ef
parscr <parscalculator@gmail.com>
2021-02-26 13:21:44 +0330

    Update rd.go

--- functions/rd.go
+++ functions/rd.go
@@ -7,12 +7,12 @@ import {
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-    name := "World"
-    
     return &events.APIGatewayProxyResponse{
         StatusCode:        200,
-        Headers:           map[string]string{Location: "https://google.com",'Cache-Control': 'no-cache',},
-        Body:       "Hi " + name,
+        Headers:           map[string]string{"Content-Type": "text/plain"},
+        MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+        Body:              "Hello, World!",
+        IsBase64Encoded:   false,
     }, nil
 }
 

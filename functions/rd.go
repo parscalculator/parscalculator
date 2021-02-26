c98f9dc5108fc2800cadbf50ccb043c259d852f7
parscr <parscalculator@gmail.com>
2021-02-26 11:28:58 +0330

    pcr

--- functions/rd.go
+++ functions/rd.go
@@ -7,18 +7,6 @@ import (
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-    req, err := http.NewRequest("GET", "https://www.google.com", nil)
-    if err != nil {
-        panic(err)
-    }
-
-    client := new(http.Client)
-    response, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(ioutil.ReadAll(response.Body))
-
     return &events.APIGatewayProxyResponse{
         StatusCode:        302,
         Headers:           map[string]string{Location: redirectUrl,'Cache-Control': 'no-cache',},

183ced906e5ca4cac5e26be151d7e7ba091e4f0d
parscr <parscalculator@gmail.com>
2021-02-26 13:17:11 +0330

    Update rd.go

--- functions/rd.go
+++ functions/rd.go
@@ -4,6 +4,9 @@ import (
     "fmt"
     "io/ioutil"
     "net/http"
+    "context"
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {

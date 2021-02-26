c6520b0050ed8693144d83961d68a4d1190a3c10
parscr <parscalculator@gmail.com>
2021-02-26 17:17:26 +0330

    Update main.go

--- main.go
+++ main.go
@@ -11,13 +11,16 @@ import (
     "context"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
    url := "https://api.idpay.ir/v1.1/payment"
+
+lc, ok := lambdacontext.FromContext(ctx)
+cc := lc.ClientContext
 
 data := map[string]string{
   "order_id": "101",
   "amount":   "10000",
-  "name":     "قاسم رادمان",
+  "name":     "cc.Client.AppTitle",
   "phone":    "09382198592",
   "mail":     "my@site.com",
   "desc":     "توضیحات پرداخت کننده",

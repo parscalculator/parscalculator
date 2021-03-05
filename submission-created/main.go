64006f44859d9d1982d74c340d2d5d6d6cee99f7
parscr <parscalculator@gmail.com>
2021-03-05 17:47:18 +0330

    Update main.go

--- submission-created/main.go
+++ submission-created/main.go
@@ -14,6 +14,7 @@ import (
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
     params := request.QueryStringParameters
    url := "https://api.idpay.ir/v1.1/payment"
+    fmt.Printf("id: %s, link: ", request)
 
 
 
